50f47b81230c8ca19dc4f9a0c799b14d7610032e
Soly-M-27 <solymar227@icloud.com>
2022-05-20 18:27:30 +0000

    Added route to handle 404

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
+from flask import make_response
 from models import storage
 
 @app.route('/status')
@@ -11,6 +12,9 @@ def status():
 def stats():
     return storage(jsonify({'{}'.format(storage.get()): '{}'.format(storage.count())}))
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     app.run(HBNB_API_HOST = "0.0.0.0", HBNB_API_PORT = 5000, threaded = True)
