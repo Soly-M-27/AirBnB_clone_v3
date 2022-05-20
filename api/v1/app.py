e14054d1e43de56ef0c93cdf1ea5354168feee59
Soly-M-27 <solymar227@icloud.com>
2022-05-20 20:02:49 +0000

    Added comments

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,12 @@ from api.v1.views import app_views
 
 app = Flask("app_views", __name__)
 
+@app.teardown_appcontext
+''' storage.close() '''
+def close():
+    storage.close()
+
+''' Check host and port with getenv '''
 host = getenv("HBNB_API_HOST")
 port = getenv("HBNB_API_PORT")
 
@@ -14,11 +20,6 @@ if not host:
     host = "0.0.0.0"
 if not port:
     port = "5000"
-
-@app.teardown_appcontext
-''' storage.close() '''
-def close():
-    storage.close()
 
 if __name__ == "__main__":
     app.run(host=host, port=port, threaded = True)
