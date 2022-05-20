e08319c97019eaeba5ec05560bed4b17971fccdf
Soly-M-27 <solymar227@icloud.com>
2022-05-20 18:22:03 +0000

    Create an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views
+from models import storage
 
 @app.route('/status')
 def status():
     return app_views(jsonify({'status': 'OK'}))
+
+@app.route('/api/v1/stats')
+def stats():
+    return storage(jsonify({'{}'.format(storage.get()): '{}'.format(storage.count())}))
+
+
+if __name__ == "__main__":
+    app.run(HBNB_API_HOST = "0.0.0.0", HBNB_API_PORT = 5000, threaded = True)
