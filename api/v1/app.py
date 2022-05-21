fee08d654c53e869ad4a20c0a6988f70aba77e74
Soly-M-27 <solymar227@icloud.com>
2022-05-21 18:33:06 +0000

    Added method for 404 handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 ''' Your first endpoint (route) will be to return the status of your API '''
 
-from flask import Flask, abort, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def close(self):
     ''' method to close all storage '''
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    ''' method to handle 404 error '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
