56e064c54a68dfd6cfc7f4d5acbf061c7f4d6443
Soly-M-27 <solymar227@icloud.com>
2022-05-21 18:00:04 +0000

    Fixed imports and their order

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,12 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models import State, User, Amenity, Review, Place, City
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.user import User
+from models.review import Review
 
 
 @app_views.route('/status')
@@ -15,6 +20,8 @@ def status():
 
 @app_views.route('/stats')
 def get_count():
+    ''' endpoint that retrieves the number of each
+    objects by type '''
     stats = {
          "amenities": storage.count(Amenity),
          "cities": storage.count(City),
