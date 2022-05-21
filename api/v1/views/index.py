f4f5779dc45333e943984ed4f954c7bf00428c2d
Soly-M-27 <solymar227@icloud.com>
2022-05-21 17:55:56 +0000

    Added method to deal stat route and get its data, returning it in JSON format

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,23 @@
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
+from models import State, User, Amenity, Review, Place, City
 
 
 @app_views.route('/status')
 def status():
     ''' route status test that return a JSON query '''
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def get_count():
+    stats = {
+         "amenities": storage.count(Amenity),
+         "cities": storage.count(City),
+         "places": storage.count(Place),
+         "reviews": storage.count(Review),
+         "states": storage.count(State),
+         "usres": storage.count(User)
+    }
+    return jsonify(stats)
