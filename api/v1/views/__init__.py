c29a1f993d5a7818822889935c41faaf64c329e6
Soly-M-27 <solymar227@icloud.com>
2022-05-23 18:19:24 +0000

    test

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-''' init file '''
+"""
+init file
+"""
 
 from flask import Blueprint
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
@@ -10,3 +12,4 @@ import api.v1.views.amenities
 import api.v1.views.users
 import api.v1.views.places
 import api.v1.views.places_reviews
+import api.v1.views.places_amenities
