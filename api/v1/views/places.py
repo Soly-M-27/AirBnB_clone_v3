6000697ef5573a13274f4f7009e92b263fe41e15
Soly-M-27 <solymar227@icloud.com>
2022-05-23 18:11:17 +0000

    Testing

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-''' Create a new view for Places objects that handles all
-RESTful API actions '''
+''' Module that handles all default RESTFul API '''
 
-from flask import request, abort, Response, jsonify
+from flask import jsonify, abort, request, Response
 from models import storage
 from models.city import City
 from models.user import User
@@ -10,15 +9,15 @@ from models.place import Place
 from api.v1.views import app_views
 
 
-@app_views.route('/cities/<city_id>/places',
-                 methods=['GET', 'POST'], strict_slashes=False)
-def get_all_place_obj(city_id=None):
-    ''' Retrieves the list of all Places objects '''
+@app_views.route('/cities/<city_id>/places', methods=['GET', 'POST'],
+                 strict_slashes=False)
+def get_places(city_id=None):
+    """ focus on all the place objects """
     city = storage.get(City, city_id)
     if not city:
         abort(404)
 
-    if request.method == "POST":
+    if request.method == 'POST':
         HTTP_body = request.get_json()
         if not HTTP_body:
             return Response("Not a JSON", 400)
@@ -26,46 +25,45 @@ def get_all_place_obj(city_id=None):
             return Response("Missing user_id", 400)
         if 'name' not in HTTP_body:
             return Response("Missing name", 400)
-
-        user_check = storage.get(User, HTTP_body.get('user_id'))
-        if not user_check:
+        user = storage.get(User, HTTP_body.get('user_id'))
+        if not user:
             abort(404)
+        place = Place(city_id=city.id, user_id=user.id,
+                      name=HTTP_body.get('name'))
+        place.save()
+        return jsonify(place.to_dict()), 201
 
-        new_place = Place(city_id=city.id, user_id=user.id,
-                          name=HTTP_body.get('name'))
-        new_place.save()
-        return (jsonify(new_place.to_dict()), 201)
+    all_places = city.places
+    places = []
 
-    All_Places = city.places
-    Existing_Places = []
-
-    for p in All_Places:
-        Existing_Places.append(p.to_dict())
-    return (jsonify(Existing_Places), 200)
+    for place in all_places:
+        places.append(place.to_dict())
+    return jsonify(places), 200
 
 
 @app_views.route('/places/<place_id>', methods=['GET', 'DELETE', 'PUT'],
                  strict_slashes=False)
-def get_a_place(place_id=None):
-    ''' Retrives, Deleted or Updates a specified Place '''
-    a_place = storage.get(Place, place_id)
-    if a_place is None:
+def get_place(place_id=None):
+    """ focus only in a single place object """
+    place = storage.get(Place, place_id)
+    if place is None:
         abort(404)
 
-    if request.method == "DELETE":
-        storage.delete(a_place)
+    if request.method == 'DELETE':
+        storage.delete(place)
         storage.save()
-        return (jsonify({}), 200)
+        return jsonify({}), 200
 
-    if request.method == "PUT":
+    if request.method == 'PUT':
         HTTP_body = request.get_json()
         if not HTTP_body:
             return Response("Not a JSON", 400)
-        HTTP_body['id'] = a_place.id
-        HTTP_body['user_id'] = a_place.user_id
-        HTTP_body['city_id'] = a_place.city_id
-        HTTP_body['created_at'] = a_place.created_at
-        a_place.__init__(**HTTP_body)
-        a_place.save()
-        return (jsonify(a_place.to_dict()), 200)
-    return (jsonify(a_place.to_dict()), 200)
+        HTTP_body['id'] = place.id
+        HTTP_body['user_id'] = place.user_id
+        HTTP_body['city_id'] = place.city_id
+        HTTP_body['created_at'] = place.created_at
+        place.__init__(**data)
+        place.save()
+        return jsonify(place.to_dict()), 200
+
+    return jsonify(place.to_dict()), 200
